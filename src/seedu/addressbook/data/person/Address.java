cb013037f29208233bdae0516971b933eb245759
Aussiroth <aussiroth@gmail.com>
2018-02-03 18:39:10 +0800

    Changed Address to consist of classes Block, Street, Unit, Postal. Added simple input check to ensure address input is in order of BLOCK, STREET, UNIT, POSTAL.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,12 +9,15 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Address {
 
     public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses should be in the format BLOCK, STREET, UNIT, POSTAL CODE";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
     private boolean isPrivate;
 
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private Postal postal;
     /**
      * Validates given address.
      *
@@ -26,7 +29,14 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        String[] parsedAddress = address.split(",");
+        if (parsedAddress.length != 4){
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
+        block = new Block(parsedAddress[0]);
+        street = new Street(parsedAddress[1]);
+        unit = new Unit(parsedAddress[2]);
+        postal = new Postal(parsedAddress[3]);
     }
 
     /**
@@ -38,22 +48,75 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return block.toString() + ", " + street.toString() + ", " + unit.toString() + ", " + postal.toString();
     }
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+        if (other == this) {
+            return true;
+        }
+        if (other instanceof Address){
+            //cheat a little since address are equal iff their string representation is equal
+            return this.toString().equals(other.toString());
+        }
+        return false;
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return block.hashCode() ^ street.hashCode() ^ unit.hashCode() ^ postal.hashCode();
     }
 
     public boolean isPrivate() {
         return isPrivate;
     }
+}
+
+class Block{
+    public final String blockValue;
+
+    public Block(String block){
+        this.blockValue = block;
+    }
+
+    public String toString() {return blockValue;}
+
+    public int hashCode() { return blockValue.hashCode();}
+}
+
+class Street{
+    public final String streetValue;
+
+    public Street(String street){
+        this.streetValue = street;
+    }
+
+    public String toString() {return streetValue;}
+
+    public int hashCode() { return streetValue.hashCode();}
+}
+
+class Unit{
+    public final String unitValue;
+
+    public Unit(String unit){
+        this.unitValue = unit;
+    }
+
+    public String toString() {return unitValue;}
+
+    public int hashCode() { return unitValue.hashCode();}
+}
+
+class Postal{
+    public final String postalValue;
+
+    public Postal(String postal){
+        this.postalValue = postal;
+    }
+
+    public String toString() {return postalValue;}
+
+    public int hashCode() { return postalValue.hashCode();}
 }
