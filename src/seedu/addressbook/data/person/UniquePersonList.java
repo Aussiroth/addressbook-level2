a3c1748cbdc162f223b423701d322efa26b94614
Alvin Yan <aussiroth@gmail.com>
2018-02-13 11:57:40 +0800

    Add method to return sorted addressbook list.

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -81,6 +81,11 @@ public class UniquePersonList implements Iterable<Person> {
         return Collections.unmodifiableList(internalList);
     }
 
+    public List<ReadOnlyPerson> sortedImmutableListView(){
+        Collections.sort(internalList);
+        return Collections.unmodifiableList(internalList);
+    }
+
 
     /**
      * Checks if the list contains an equivalent person as the given argument.
